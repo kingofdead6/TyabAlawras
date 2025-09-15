608a69966538b910676381522461ea7747103e9b
kingofdead6 <ny_kahlouche@esi.dz>
2025-09-15 10:34:33 +0100

    Added new features

--- client/src/components/Shared/Footer.jsx
+++ client/src/components/Shared/Footer.jsx
@@ -174,7 +174,7 @@ export default function Footer() {
 
                 <div className="flex items-center">
                   <a
-                    href="/download.html"
+                    href="https://github.com/kingofdead6/TyabAlawras/download.html"
                     className="px-6 py-3 bg-yellow-400 text-black rounded-lg hover:bg-yellow-600 transition font-semibold"
                   >
                     حمل التطبيق
